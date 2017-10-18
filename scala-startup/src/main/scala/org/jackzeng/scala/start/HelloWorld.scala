dba3f30a6c6984633c7feb57911ea234cb2efe06
zengxijin <zengxijin@qq.com>
2017-10-18 15:26:31 +0800

    add scala project

---
+++ scala-startup/src/main/scala/org/jackzeng/scala/start/HelloWorld.scala
@@ -0,0 +1,12 @@
+package org.jackzeng.scala.start
+
+/**
+  * Created by zengxj on 17-10-18
+  */
+class HelloWorld {
+
+  def main(args: Array[String]): Unit = {
+    println("class hello world")
+  }
+
+}
---
+++ scala-startup/src/main/scala/org/jackzeng/scala/start/HelloWorldObject.scala
@@ -0,0 +1,12 @@
+package org.jackzeng.scala.start
+
+/**
+  * Created by zengxj on 17-10-18
+  */
+object HelloWorldObject {
+
+  def main(args: Array[String]): Unit = {
+    println("hi, jack welcome to scala")
+  }
+
+}
